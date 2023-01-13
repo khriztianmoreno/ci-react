72041170b20920bca1b272eb242e1a56c8b2be19
khriztianmoreno <khriztianmoreno@gmail.com>
2023-01-13 15:38:40 -0500

    ci: fail test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event'
 import App from './App';
 
 test('hello world', () => {
-  expect(true).toBe(true);
+  expect(true).toBe(false);
 });
 
 test('renders Free dictionary title', () => {
